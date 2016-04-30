14c758cf038c10b58146007e0b8a3a2aad88f202
jonmervine <=>
2016-04-30 18:05:20 -0400

    Initial commit
    lets get this rolling

---
+++ src/main/Driver.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by Shirobako on 4/23/2016.
+ */
+public class Driver {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/FileNavigator.java
@@ -0,0 +1,7 @@
+package main;
+
+/**
+ * Created by Shirobako on 4/24/2016.
+ */
+public class FileNavigator {
+}
