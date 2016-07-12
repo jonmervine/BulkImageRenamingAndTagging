22a65ed75650f95df87aa8432859a176bbd6b0a4
jonmervine <=>
2016-07-12 00:55:50 -0400

    Getting closer to parsing out the table. Similarity, Resolution, Rating, and even the url. Just need to break out the tags

--- src-test/main/FileNavigatorTest.java
+++ src-test/main/FileNavigatorTest.java
@@ -29,27 +29,125 @@ public class FileNavigatorTest {
             File file = new File("D:\\Downloads\\derp\\Album Fqedd - Imgur\\029 - UESTINJ.jpg");
             if (file.exists() && file.isFile()) {
                 Document doc = Jsoup.connect("http://iqdb.org/").data("file", file.getName(), new FileInputStream(file)).post();
+                //doc is the entire raw html page
 
                 Elements elements = doc.getElementsByTag("table");
+                //elements is a set of all of the <table> elements
 
                 for (Element element : elements) {
-                    Elements similarity = element.getElementsContainingText("Best Match");
-                    if (similarity.size() > 0) {
-                        for (Element ele2 : element.getAllElements()) {
+                    //iterating through all the table elements. First one is your uploaded image
+                    //Last <td> is the image dimension
 
+                    //Each table is one "card" on iqdb so only the second one will be the "best match"
+                    //Second element will provide all the information you need. going into the similarity and all that is a waste of time
 
-                            if (ele2.getElementsByAttributeValueContaining("src", "danbooru.donmai").size() > 0) {
-                                Elements derpy = ele2.getElementsByAttributeValueContaining("src", "danbooru.donmai");
-                                System.out.println("derppppp");
-                            }
-                        }
+                    //Use the similarity search for Best Match to identify the element that is best match if you don't trust the 2nd.
+
+                    //can similarly search for "Additional match" to find all teh additional matches. Anything beyond additional matches is a waste of time.
+
+                    Elements noMatch = element.getElementsContainingText("No relevant matches");
+                    if (noMatch.size() > 0 ) {
+                        System.out.println("There was no relevant match for this");
+                        continue;
+                        //todo we should probably move this image to a holding folder of some kind
+                    }
+
+                    Elements bestMatch = element.getElementsContainingText("Best Match");
+                    if (bestMatch.size() > 0) {
+                        processTable(element);
+                    }
+
+                    Elements additionalMatches = element.getElementsContainingText("Additional match");
+                    if (additionalMatches.size() > 0) {
+                        //do nothing for now
                     }
                 }
             }
 
         } catch (IOException e) {
-            System.out.println("shit fucked up");
+            System.out.println("shit fucked up" + e.getMessage() + e.getCause() + e.getStackTrace());
         }
+
+    }
+
+    private void processTable(Element element) {
+        Elements tableDatas = element.getElementsByTag("td");
+        if (tableDatas.size() != 4) {
+            System.out.println("the table should have 4 table datas what the fuck happened");
+            System.out.println(tableDatas.toString());
+        }
+
+        Element image = tableDatas.get(0);
+        Element resolutionAndRating = tableDatas.get(2); //Safe, Ero, Explicit
+        Element similarityPercent = tableDatas.get(3);
+        int similarity = -1;
+        try {
+            String similarityText = similarityPercent.text();
+            similarity = Integer.parseInt(similarityText.substring(0, similarityText.indexOf("%")));
+        } catch (NumberFormatException ex) {
+            System.out.println("EXCEPTION WHILE PARSING SIMILARITY: " + similarityPercent.text());
+        }
+
+        String resolutionAndRatingText = resolutionAndRating.text();
+        String rating = resolutionAndRatingText.substring(resolutionAndRatingText.indexOf("[") + 1, resolutionAndRatingText.indexOf("]"));
+        String resolution = resolutionAndRating.text().substring(0, resolutionAndRating.text().indexOf(" ["));
+
+        System.out.println(image.getElementsByTag("a").get(0).attr("href").toString());
+        System.out.println(image.getElementsByTag("img").get(0).attr("alt").toString());
+
+
+
+/*
+<table><th>Best match</th>
+<td class='image'><a href="//konachan.com/post/show/194975"><img src='/konachan/a/4/f/a4fea7399aad1d398047020b06c954e3.jpg' alt="Rating: s Score: 164 Tags: animal bikini_top breasts bubbles cleavage fish kagayan1096 long_hair mermaid purple_eyes purple_hair twintails underwater vocaloid voiceroid water yuzuki_yukari" title="Rating: s Score: 164 Tags: animal bikini_top breasts bubbles cleavage fish kagayan1096 long_hair mermaid purple_eyes purple_hair twintails underwater vocaloid voiceroid water yuzuki_yukari" width='150' height='105'></a></td>
+<td><img alt="icon" src="//konachan.com/favicon.ico" class="service-icon">Konachan</td>
+<td>1800×1254 [Safe]</td>
+<td>96% similarity</td></tr></table></div>
+
+<table><tr><th>Additional match</th>
+<td class='image'><a href="http://www.zerochan.net/1881288"><img src='/zerochan/5/0/d/50d7365a05e981ec7480116ef4b94fe6.jpg' alt="Rating: q Tags: Female, Ecchi, Fanart, Water, Fish, Long Hair, Ocean, Purple Eyes, Animal, Purple Hair, Clenched Hand, Vocaloid, Mermaid, Beads, Bubble, Underwater, Jewelry, Coral, Pearls, Heart, Kemonomimi, Open Mouth, Outdoors, Pixiv, Midriff, Solo, Frame, Bikini Top, Text, Head Fins, Alternate Setting, Sidelocks, Hand on Chest, Sleeveless, Black Border, Fanart From Pixiv, Looking At Camera, English Text, Coral Reef, Blurry, Water Bubbles, Yuzuki Yukari, Voiceroid, Cleavage, Shoulder Strap, Pixiv Id 4703380, Exposed Shoulders" title="Rating: q Tags: Female, Ecchi, Fanart, Water, Fish, Long Hair, Ocean, Purple Eyes, Animal, Purple Hair, Clenched Hand, Vocaloid, Mermaid, Beads, Bubble, Underwater, Jewelry, Coral, Pearls, Heart, Kemonomimi, Open Mouth, Outdoors, Pixiv, Midriff, Solo, Frame, Bikini Top, Text, Head Fins, Alternate Setting, Sidelocks, Hand on Chest, Sleeveless, Black Border, Fanart From Pixiv, Looking At Camera, English Text, Coral Reef, Blurry, Water Bubbles, Yuzuki Yukari, Voiceroid, Cleavage, Shoulder Strap, Pixiv Id 4703380, Exposed Shoulders" width='150' height='116'></a></td>
+<td><img alt="icon" src="http://zerochan.net/favicon.ico" class="service-icon">Zerochan</td>
+<td>1800×1398 [Ero]</td>
+<td>65% similarity</td></tr></table></div>
+
+<table><tr><th>Additional match</th>
+<td class='image'><a href="//danbooru.donmai.us/posts/1905445"><img src='/danbooru/5/0/d/50d7365a05e981ec7480116ef4b94fe6.jpg' alt="Rating: s Tags: 1girl bikini_top coral fish highres kagayan1096 long_hair mermaid monster_girl purple_eyes purple_hair twintails underwater vocaloid voiceroid yuzuki_yukari" title="Rating: s Tags: 1girl bikini_top coral fish highres kagayan1096 long_hair mermaid monster_girl purple_eyes purple_hair twintails underwater vocaloid voiceroid yuzuki_yukari"></a></td>
+<td><img alt="icon" src="//danbooru.donmai.us/favicon.ico" class="service-icon">Danbooru <span class="el"><a href="http://gelbooru.com/index.php?page=post&s=list&md5=50d7365a05e981ec7480116ef4b94fe6"><img alt="icon" src="http://gelbooru.com/favicon.png" class="service-icon">Gelbooru</a></span></td>
+<td>1800×1398 [Safe]</td>
+<td>64% similarity</td></tr></table></div>
+
+<div><table><tr><th>Additional match</th>
+<td class='image'><a href="//chan.sankakucomplex.com/post/show/4376911"><img src='/sankaku/5/0/d/50d7365a05e981ec7480116ef4b94fe6.jpg' alt="Rating: s Tags: {464292,79222,337507,599,49,455,454379,6878,9081,47632,130462,446,399,26640,7068,1356,2818,330,3885,322,97161,142,5154,1349,107573,73719,21749,1090,34240,124887,264387,40213,161268,563,8503,163,1091,771,5909,2084,5212,95705,43623,100652,64721,108,555145,582845,171086,79775,571}" title="Rating: s Tags: {464292,79222,337507,599,49,455,454379,6878,9081,47632,130462,446,399,26640,7068,1356,2818,330,3885,322,97161,142,5154,1349,107573,73719,21749,1090,34240,124887,264387,40213,161268,563,8503,163,1091,771,5909,2084,5212,95705,43623,100652,64721,108,555145,582845,171086,79775,571}" width='150' height='116'></a></td>
+<td><img alt="icon" src="//images.sankakucomplex.com/gfx/favicon.png" class="service-icon">Sankaku Channel</td>
+<td>1800×1398 [Safe]</td>
+<td>63% similarity</td></tr></table>
+
+NO TAGS
+<table><tr><th>Additional match</th>
+<td class='image'><a href="http://e-shuushuu.net/image/559324/"><img src='/e-shuushuu/a/a/c/aac4bc16f0672c493063a42fd23f80a4.jpg' alt="[IMG]" width='106' height='150'></a></td>
+<td><img alt="icon" src="http://e-shuushuu.net/special/favicon.ico" class="service-icon">e-shuushuu</td>
+<td>640×905 [Safe]</td>
+<td>96% similarity</td></tr></table></div>
+
+NO TAGS
+<table><tr><th>Additional match</th>
+<td class='image'><a href="http://www.theanimegallery.com/image/39945"><img src='/anigal/f/0/2/f02ef2955da8f80523b0bb2f86da5dca.jpg' alt="Rating: s Score: 9.40" title="Rating: s Score: 9.40" width='150' height='112'></a></td>
+<td><img alt="icon" src="http://www.theanimegallery.com/favicon.ico" class="service-icon">The Anime Gallery</td>
+<td>800×600 [Safe]</td>
+<td>97% similarity</td></tr></table></div>
+
+<table><tr><th>Additional match</th>
+<td class='image'><a href="http://gelbooru.com/index.php?page=post&s=view&id=2030833"><img src='/gelbooru/2/7/2/272cc7cf8419b9399a8adc74c9837e8d.jpg' alt="Rating: s Score: 5 Tags: blue_hair blush breast_smother breasts eyes_closed huge_breasts lying ryofu_housen_(sangoku_hime) sangoku_hime_3 silver_hair uwami_risuke" title="Rating: s Score: 5 Tags: blue_hair blush breast_smother breasts eyes_closed huge_breasts lying ryofu_housen_(sangoku_hime) sangoku_hime_3 silver_hair uwami_risuke" width='150' height='84'></a></td>
+<td><img alt="icon" src="http://gelbooru.com/favicon.png" class="service-icon">Gelbooru</td>
+<td>1280×720 [Safe]</td>
+<td>94% similarity</td></tr></table></div>
+
+<table><tr><th>Additional match</th>
+<td class='image'><a href="https://yande.re/post/show/335286"><img src='/moe.imouto/c/3/a/c3a8f653c4c86cacf00a8dd9da9539c8.jpg' alt="Rating: q Score: 17 Tags: ass bandaid kirisaki_chitoge nisekoi onodera_kosaki school_swimsuit swimsuits watanabe_akio" title="Rating: q Score: 17 Tags: ass bandaid kirisaki_chitoge nisekoi onodera_kosaki school_swimsuit swimsuits watanabe_akio" width='150' height='84'></a></td>
+<td><img alt="icon" src="https://yande.re/favicon.ico" class="service-icon">yande.re</td>
+<td>1920×1080 [Ero]</td>
+<td>95% similarity</td></tr></table></div>
+ */
 
     }
 
